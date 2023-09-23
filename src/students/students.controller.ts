2fefc0f65deb757075b452bd13333dc386690d6a
Pseudonyme <aghatorzz@gmail.com>
2023-09-23 18:01:37 +0200

    ajout de qlqs fcts

--- src/students/students.controller.ts
+++ src/students/students.controller.ts
@@ -1,5 +1,13 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { CreateStudentDto } from 'src/DTO/create-student.dto';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateStudentDto } from 'src/DTOs/create-student.dto';
 import { StudentsService } from './students.service';
 import { Student } from 'src/Entities/student.entity';
 
@@ -17,5 +25,21 @@ export class StudentsController {
     // Call the UserService to create the user
     const createdUser = await this.studentsService.createStudent(createUserDto);
     return createdUser;
+  }
+
+  @Put(':id')
+  async updateStudent(@Param('id') id: number, @Body() updatedData: any) {
+    return this.studentsService.updateStudent(id, updatedData);
+  }
+
+  @Get(':id')
+  async getStudentById(@Param('id') id: number) {
+    return this.studentsService.getStudentById(id);
+  }
+
+  @Delete(':id')
+  async deleteStudentById(@Param('id') id: number) {
+    await this.studentsService.deleteStudentById(id);
+    return { message: 'Student deleted successfully' };
   }
 }
--- src/students/students.service.ts
+++ src/students/students.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { CreateStudentDto } from 'src/DTO/create-student.dto';
+import { CreateStudentDto } from 'src/DTOs/create-student.dto';
 import { supabase } from 'supabase.config';
 
 @Injectable()
@@ -25,5 +25,40 @@ export class StudentsService {
       throw error;
     }
     return data;
+  }
+
+  async updateStudent(id: number, updatedData: any): Promise<any> {
+    const { data, error } = await supabase
+      .from('Students')
+      .update(updatedData)
+      .eq('id', id);
+
+    if (error) {
+      throw error;
+    }
+
+    return data;
+  }
+
+  async getStudentById(id: number): Promise<any> {
+    const { data, error } = await supabase
+      .from('Students')
+      .select('*')
+      .eq('id', id)
+      .single();
+
+    if (error) {
+      throw error;
+    }
+
+    return data;
+  }
+
+  async deleteStudentById(id: number): Promise<void> {
+    const { error } = await supabase.from('Students').delete().eq('id', id);
+
+    if (error) {
+      throw error;
+    }
   }
 }
